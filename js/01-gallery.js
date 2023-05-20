f7c02c88ed6ca5377349fbe6cf509d1fc318c663
AnonymousMar <119819802+AnonymousMar@users.noreply.github.com>
2023-05-20 16:52:53 +0200

    ....

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,6 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const galleryEl = document.querySelector('.gallery');
-const bodyEl = document.querySelector('.body');
 let modalImage;
 
 
@@ -21,34 +20,31 @@ galleryEl.insertAdjacentHTML("beforeend", initGallery);
 const galleryClick = event => {
     event.preventDefault();
 
-    if (event.target.nodeNmae !== "IMG") {
+    if (event.target.nodeName !== "IMG") {
         return event;
     }
     onOpenModal(event.target.dataset.source);
 }
 galleryEl.addEventListener('click', galleryClick);
 
-const onCreateModal = img => basicLightbox.create(`<img src="${img}" width="1280" alt="${img}">`);
+const onCreateModal = img => basicLightbox.create(`<img src="${img}" width="1280" alt="${img}">`, {
+    onShow: () => { document.addEventListener("keydown", onKeyPress); },
+    onClose: () => { document.removeEventListener("keydown", onKeyPress); }
+     });
 
 const onOpenModal = img => {
     modalImage = onCreateModal(img);
     modalImage.show();
     console.log("Open modal");
-    document.addEventListener("keyup", onKeyPress);
 };
 
 const onCloseModal = event => {
      if (event === "click") modalImage.close();
     console.log("Close modal with click");
-    document.removeEventListener("click", onKeyPress);
  };
-
 
 const onKeyPress = event => {
     if (event.code === "Escape") modalImage.close();
-    console.log("Close modal with escape");
-    document.removeEventListener("keyup", onKeyPress);
 };
-
 
 console.log(galleryItems);
